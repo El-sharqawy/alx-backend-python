bb46c52c2fdb354fe16aa1668bae16153e57914b
El-sharqawy <elsharqawy2@gmail.com>
2024-08-01 16:12:22 +0300

    Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+a type-annotated function add that
+takes a float a and a float b as arguments
+and returns their sum as a float.
+"""
+
+
+def add(x: float, y: float) -> float:
+    """Calcualtes the SUM of X and Y"""
+    return x + y

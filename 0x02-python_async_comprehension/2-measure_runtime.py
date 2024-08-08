b11be62a21c02e6c20b9ac8cc2b98f3475906957
El-sharqawy <elsharqawy2@gmail.com>
2024-08-08 03:11:45 +0300

    Measure Runtime

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""async parallel comprehension script"""
+
+import time
+import asyncio
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Run time function for four parallel comprehensions"""
+    start = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+    )
+    end = time.time()
+    return end - start
